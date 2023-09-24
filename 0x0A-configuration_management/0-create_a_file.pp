32608f5e8151464a851d73adec0b8fd20dde4f9b
Lionel <ndabagajeanlionel@gmail.com>
2023-09-24 19:45:21 +0000

    creation of a file in  in /tmp successfully

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# This is a file resource declaration
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
