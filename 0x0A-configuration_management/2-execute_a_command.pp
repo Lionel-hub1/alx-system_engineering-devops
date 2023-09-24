04f7f22fc7d0cfd41437cecbe8944473c8bf6d1c
Lionel <ndabagajeanlionel@gmail.com>
2023-09-24 20:47:29 +0000

    Killing the process successfully

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,13 @@
 # The code below will kill the process with the name killmenow.
 
+file { '/tmp/killmenow':
+  ensure  => file,
+  content => "This file ensures the killmenow process can be terminated.",
+}
+
 exec { 'killmenow':
-  command  => '/usr/bin/pkill killmenow',
-  provider => 'shell',
-  returns  => [0, 1],
+  command     => 'pkill killmenow',
+  path        => '/usr/bin:/bin',
+  refreshonly => true,
+  subscribe   => File['/tmp/killmenow'],
 }
