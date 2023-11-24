c0dc67367f47a73a3106cc979951281925ffeb9b
Lionel <ndabagajeanlionel@gmail.com>
2023-11-24 15:36:54 +0000

    Creating a file in /tmp/school successfully

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# This script will create a file called /tmp/school with the content "I love Puppet"
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
