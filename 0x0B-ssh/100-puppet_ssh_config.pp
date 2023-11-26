cf706794359a47729e9f44f57433cd5ecd391f11
Lionel <ndabagajeanlionel@gmail.com>
2023-11-26 21:29:40 +0000

    Changed some things wo work

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,14 @@
 # This puppet manifest will install the ssh key for the school user
-file { '/home/vagrant/.ssh/config':
-    ensure => present,
-    content => "
-        Host *
-            IdentityFile ~/.ssh/school
-            PasswordAuthentication no
-    ",
+# Disable password authentication
+file_line { 'Turn off passwd auth':
+    path    => '/etc/ssh/ssh_config',
+    line    => 'PasswordAuthentication no',
+    match   => '^#?PasswordAuthentication',
+}
+
+# Set the identity file
+file_line { 'Declare identity file':
+    path    => '/etc/ssh/ssh_config',
+    line    => 'IdentityFile ~/.ssh/school',
+    match   => '^#?IdentityFile',
 }
