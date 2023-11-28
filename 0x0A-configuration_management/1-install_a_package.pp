65c7140f9aef857295b685a295c48ea14ddf94b8
Lionel <ndabagajeanlionel@gmail.com>
2023-11-28 16:02:26 +0000

    added werkzeug too beside flask installation

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-# This script will install the version 2.1.0 of the Flask Python package
+# This script will install the version 2.1.0 of the Flask Python package and the version 2.1.1 of the Werkzeug Python package.
 
 package { 'flask':
   ensure   => '2.1.0',
   provider => pip3,
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
 }
